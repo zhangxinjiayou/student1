6d7fd8b1f56514ce8912c452d690701fff37ab7b
zhangxin <zhangxin@qq.com>
2020-11-02 17:21:22 +0800

    在家开发签到功能剩余50%

---
+++ SignIn.java
@@ -0,0 +1,13 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-02 17:06:45
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-02 17:20:38
+*/
+
+public class SignIn {
+    private String name;
+    public void ssig(){
+        SSS
+    }
+}
