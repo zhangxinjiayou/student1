2aab86d3e59998824e560d925e3145826cabb404
zx <zx@qq.com>
2020-11-03 09:24:58 +0800

    完成签退功能开发

---
+++ SingOut.java
@@ -0,0 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-02 17:12:35
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-03 09:30:43
+*/
+
+public class SignOut {
+    private int id;
+    private String name;
+
+}
